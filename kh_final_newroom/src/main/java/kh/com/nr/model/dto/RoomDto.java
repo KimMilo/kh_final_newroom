a28d92f7ded4325f947585b2e362413eab9c10ff
user1 <user1@DESKTOP-UU1NLVA>
2023-03-21 14:50:28 +0900

    root-context, servlet-context 해결 및 chat-mapper 수정중

--- kh_final_newroom/src/main/java/kh/com/nr/model/dto/RoomDto.java
+++ kh_final_newroom/src/main/java/kh/com/nr/model/dto/RoomDto.java
@@ -1,35 +1,22 @@
 package kh.com.nr.model.dto;
 
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Component
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class RoomDto {
 	private int roomID; //방번호
 	private String userID; //이 채팅방에서 대화하는 사용자 아이디
 	
-	public RoomDto() {}
-
-	public RoomDto(int roomID, String userID) {
-		super();
-		this.roomID = roomID;
-		this.userID = userID;
-	}
-
-	public int getRoomID() {
-		return roomID;
-	}
-
-	public void setRoomID(int roomID) {
-		this.roomID = roomID;
-	}
-
-	public String getUserID() {
-		return userID;
-	}
-
-	public void setUserID(String userID) {
-		this.userID = userID;
-	}
-
-	@Override
-	public String toString() {
-		return "RoomDto [roomID=" + roomID + ", userID=" + userID + "]";
-	}
 }
