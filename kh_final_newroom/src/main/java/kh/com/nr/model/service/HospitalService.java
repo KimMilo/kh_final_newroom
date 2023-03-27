e8469162a8205bbbc4c4788990d2a5f0451141ec
user1 <user1@DESKTOP-UU1NLVA>
2023-03-27 12:25:02 +0900

    에러 제거

--- kh_final_newroom/src/main/java/kh/com/nr/model/service/HospitalService.java
+++
@@ -1,9 +0,0 @@
-package kh.com.nr.model.service;
-
-import java.util.List;
-
-import kh.com.nr.model.dto.HospitalDto;
-
-public interface HospitalService {
-	List<HospitalDto> getSafetyHospitalList(String houseinfoId);
-}
