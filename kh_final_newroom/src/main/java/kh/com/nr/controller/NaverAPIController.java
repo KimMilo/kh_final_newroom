e4422a3b8b52c271e1aa6768e3ec1143c8f4013b
user1 <user1@DESKTOP-UU1NLVA>
2023-03-31 15:05:19 +0900

    properties 수정

--- kh_final_newroom/src/main/java/kh/com/nr/controller/NaverAPIController.java
+++ kh_final_newroom/src/main/java/kh/com/nr/controller/NaverAPIController.java
@@ -12,12 +12,8 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.crypto.Cipher;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
